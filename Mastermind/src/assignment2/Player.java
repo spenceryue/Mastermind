ec13e1253c64cf5982e425a1794522346ba2f37c
Spencer Yue <spenceryue@yahoo.com>
2016-06-19 22:36:16 -0500

    Player class written

--- Mastermind/src/assignment2/Player.java
+++ Mastermind/src/assignment2/Player.java
@@ -1,5 +1,45 @@
 package assignment2;
 
-public class Player {
+import java.util.Scanner;
 
+public class Player {
+	private boolean ready;
+	private Scanner scan;
+
+	public Player() {
+		ready = false;
+		scan = new Scanner(System.in);
+	}
+
+	public String guess() {
+		String guess = scan.nextLine();
+		if (guess.toLowerCase().equals("history"))
+			return showHistory();
+		else
+			return guess;
+	}
+	
+	public boolean startGame() {
+		boolean asking = true;
+		while(asking) {
+			String reply = scan.next();
+			if (reply.toLowerCase().equals("y"))
+				return ready=true;
+			else if (reply.toLowerCase().equals("n"))
+				return ready=false;
+			else
+				System.out.println("Invalid choice. Please enter either Y/N.");
+		}
+		return ready;
+	}
+	
+	public boolean playAnother() {
+		if (!startGame())
+			scan.close();
+		return ready;
+	}
+	
+	public String showHistory() {
+		return null;
+	}
 }
