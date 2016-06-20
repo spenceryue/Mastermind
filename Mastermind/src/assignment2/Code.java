23b8adc80d4b9e8f275c7068fc40a16a0333c9a4
Spencer Yue <spenceryue@yahoo.com>
2016-06-20 14:13:02 -0500

    Revert ""
    This reverts commit 0ceb152e5178e624575c243a717f7f908d4b7d9e.

--- Mastermind/src/assignment2/Code.java
+++ Mastermind/src/assignment2/Code.java
@@ -1,36 +1,5 @@
 package assignment2;
 
 public class Code {
-	private static final int pegNumber = GameConfiguration.pegNumber;
-	private static final String[] colors = GameConfiguration.colors;
-	private String [] pegs = new String[pegNumber];
 
-	public Code(String[] pegs) {
-		// check validity of input (size)
-		if (pegs.length != pegNumber)
-			throw new IllegalArgumentException();
-		
-		// check validity of input (colors)
-		for (String color: pegs) {
-			boolean found = false;
-			for (String allowedColor : colors)
-				if (allowedColor.toLowerCase().startsWith(color.toLowerCase())) {
-					found = true;
-					break;
-				}
-			if (!found)
-				throw new IllegalArgumentException();
-		}
-		
-		this.pegs = pegs;
-	}
-	
-	/**
-	 * 
-	 * @param c, Code to be compared to
-	 * @return, array of booleans indicating a match or not
-	 */
-	public boolean[] compare(Code c) {
-		
-	}
 }
