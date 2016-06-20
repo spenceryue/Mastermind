4eda5e0a8009041ef9d08fe64c49b024b731f62b
Spencer Yue <spenceryue@yahoo.com>
2016-06-19 19:53:52 -0500

    initial commit

---
+++ Mastermind/src/assignment2/GameConfiguration.java
@@ -0,0 +1,16 @@
+package assignment2;
+
+/** Replace <...> with your actual data. 
+ * Spencer Yue
+ * sty223
+ * (No partner)
+ * https://github.com/spenceryue/mastermind
+ * Slip days used: <0>
+ * Summer 2016
+ */
+
+public class GameConfiguration {
+	public static final int guessNumber = 12;
+	public static final String[] colors = {"B","G","O","P","R","Y"};
+	public static final int pegNumber = 4;
+}
---
+++ Mastermind/src/assignment2/Header.java
@@ -0,0 +1,10 @@
+package assignment2;
+
+/** Replace <...> with your actual data. 
+ * Spencer Yue
+ * sty223
+ * (No partner)
+ * https://github.com/spenceryue/mastermind
+ * Slip days used: <0>
+ * Summer 2016
+ */
