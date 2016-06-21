cb615d9655209975af9b88d20f559b07c29fc06e
Spencer Yue <spenceryue@yahoo.com>
2016-06-20 21:16:41 -0500

    Finished Computer class

--- Mastermind/src/assignment2/Computer.java
+++ Mastermind/src/assignment2/Computer.java
@@ -1,5 +1,59 @@
 package assignment2;
 
-public class Computer {
+import java.util.ArrayList;
+import java.util.Random;
 
+public class Computer extends GameConfiguration {
+	Code secret;
+	Random generator;
+	
+	public Computer () {
+		generator = new Random();
+		String[] code = new String[pegNumber];
+		for (int i=0; i<pegNumber; i++) {
+			int index = generator.nextInt(colors.length);
+			code[i] = colors[index];
+		}
+		
+		secret = new Code(code);
+	}
+	
+	/**
+	 * @param c, code to check against secret
+	 * @return, array of {#black pegs, #white pegs}
+	 */
+	public int[] checkGuess(Code c) {
+		int black = 0;
+		int white = 0;
+		
+		ArrayList<String> remainingGuess = new ArrayList<String>(pegNumber);
+		ArrayList<String> remainingSecret = new ArrayList<String>(pegNumber);
+		boolean[] comparison = c.compare(secret);
+		
+		for (int i=0; i<pegNumber; i++)
+			if (comparison[i]) {
+				remainingSecret.add(secret.getPegs()[i]);
+				black++;
+			}
+			else
+				remainingGuess.add(c.getPegs()[i]);
+		
+		for (String peg : remainingGuess)
+			for (int i=0; i<remainingSecret.size(); i++)
+				if (remainingSecret.get(i).equals(peg)) {
+					remainingSecret.remove(i--);
+					white++;
+				}
+		
+		int[] result = {black, white};
+		return result;
+	}
+	
+	/**
+	 * 
+	 * @return secret Code
+	 */
+	public Code getSecret() {
+		return secret;
+	}
 }
